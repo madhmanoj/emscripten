c0221ec8c19855653307dcf235f8f8250f677c7b
Madhav Manoj <madhavmanoj@outlook.com>
2025-10-10 11:23:51 +0000

    Fix retrieval of mounted directory names

--- system/lib/wasmfs/file.cpp
+++ system/lib/wasmfs/file.cpp
@@ -208,7 +208,11 @@ int Directory::Handle::removeChild(const std::string& name) {
 
 std::string Directory::Handle::getName(std::shared_ptr<File> file) {
   if (getDir()->maintainsFileIdentity()) {
-    return getDir()->getName(file);
+    //return getDir()->getName(file);
+    auto string = getDir()->getName(file);
+    if (!string.empty()) {
+      return string;
+    }
   }
   auto& dcache = getDir()->dcache;
   for (auto it = dcache.begin(); it != dcache.end(); ++it) {
